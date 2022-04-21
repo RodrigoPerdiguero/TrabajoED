ee404a67efe428c99167cdaec37975a9b1e2cb8f
EQUIPO <EQUIPO@EQUIPO-PC.home>
2022-04-21 20:50:59 +0200

    Buenas noches

--- src/mascotas/Loro.java
+++ src/mascotas/Loro.java
@@ -29,7 +29,7 @@ class Loro extends Ave {
     }
     void saluda() {
         System.out.println("Buenos días")
-        System.out.println("Buenas nochess");
+        System.out.println("Buenas noches");
     }
 
     void volar() {
