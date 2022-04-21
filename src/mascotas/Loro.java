1fc4191c0fc6d1609fae30339b6bfdb6581928b6
EQUIPO <EQUIPO@EQUIPO-PC.home>
2022-04-21 20:09:07 +0200

    conflicto_1_L

--- src/mascotas/Loro.java
+++ src/mascotas/Loro.java
@@ -26,7 +26,7 @@ class Loro extends Ave {
 
     void habla() {
         System.out.println("HOLA HOLA");
-        System.out.println("HOLA CROCROO");
+        System.out.println("HOLA CROCROOO");
     }
 
     void saluda() {
