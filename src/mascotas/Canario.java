1db0197a9c2779d9a20fca1e64013c4ad9a6e13e
Abraham <abraham.stodomingo.gu@gmail.com>
2022-06-14 19:07:55 +0200

    añadido getNombre en habla()

--- src/mascotas/Canario.java
+++ src/mascotas/Canario.java
@@ -24,7 +24,7 @@ class Canario extends Ave {
     }
 
     void habla() {
-        System.out.println("PIO PIO");
+        System.out.println("PIO PIO" + this.getNombre());
     }
 
     void saluda() {
