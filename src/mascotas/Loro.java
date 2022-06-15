934d08e00f00695dc32591feec6eaf1b61a20a44
Abraham <abraham.stodomingo.gu@gmail.com>
2022-06-15 21:48:17 +0200

    añadido getNombre() constructor habla()

--- src/mascotas/Loro.java
+++ src/mascotas/Loro.java
@@ -26,7 +26,7 @@ class Loro extends Ave {
     }
 
     void habla() {
-        System.out.println("HOLA HOLA");
+        System.out.println("HOLA HOLA" + this.getNombre());
     }
 
     void saluda() {
